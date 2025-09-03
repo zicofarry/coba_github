892373246733644115dfe9da640d1b7e05539b87
joule.io <julian.dsatrio@gmail.com>
2025-09-03 20:30:00 +0700

    nambah file sama update java nya

--- Halo.java
+++ Halo.java
@@ -1,8 +1,11 @@
 class Halo{
     public static void main(String[] args) {
         String kata;
+        String nama = "Dunia";
 
         kata = "Halo Dunia!";
+
+        System.out.println(nama);
         System.out.println(kata);
     }
 }
